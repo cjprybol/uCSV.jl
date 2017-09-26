0402c8f49d0f1eea1d8ab599b8a49182d82ca9be
Cameron Prybol <cameron.prybol@gmail.com>
2017-09-26 08:58:41 -0700

    Stop loading StringEncodings

--- src/uCSV.jl
+++ src/uCSV.jl
@@ -1,7 +1,7 @@
 __precompile__(true)
 module uCSV
 
-using DataFrames, Nulls, CategoricalArrays, StringEncodings
+using DataFrames, Nulls, CategoricalArrays
 
 COLMAP{T} = Union{Dict{String, T}, Dict{Int, T}}
 
