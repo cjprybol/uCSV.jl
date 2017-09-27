eb730ad687c0dd1c60e021a736c03c74f86adc69
Cameron Prybol <cameron.prybol@gmail.com>
2017-09-26 20:22:13 -0700

    More Int64/32 fixes and updated make file for docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,29 @@
 using Documenter, uCSV
 
-makedocs(format   = :html,
-         sitename = "uCSV",
-         pages    = Any["Home"   => "index.md",
-                        "Manual" => Any["man/defaults.md",
-                                        "man/headers.md",
-                                        "man/delimiters.md",
-                                        "man/dataframes.md",
-                                        "man/missingdata.md",
-                                        "man/declaring-types.md",
-                                        "man/international.md",
-                                        "man/customparsers.md",
-                                        "man/quotes-escapes.md",
-                                        "man/comments-skiplines.md",
-                                        "man/malformed.md",
-                                        "man/url.md",
-                                        "man/compressed.md",
-                                        "man/categoricals.md",
-                                        "man/unsupported.md",
-                                        "man/benchmarks.md"]])
+makedocs(
+    modules = [uCSV],
+    format   = :html,
+    clean = false,
+    doctest = true,
+    sitename = "uCSV.jl",
+    pages = Any["Home" => "index.md",
+                "Manual" => Any["man/defaults.md",
+                                "man/headers.md",
+                                "man/delimiters.md",
+                                "man/dataframes.md",
+                                "man/missingdata.md",
+                                "man/declaring-types.md",
+                                "man/international.md",
+                                "man/customparsers.md",
+                                "man/quotes-escapes.md",
+                                "man/comments-skiplines.md",
+                                "man/malformed.md",
+                                "man/url.md",
+                                "man/compressed.md",
+                                "man/categoricals.md",
+                                "man/unsupported.md",
+                                "man/benchmarks.md"]])
 
-deploydocs(repo = "github.com/cjprybol/uCSV.jl.git",
-           target = "build",
-           julia = "0.6",
-           deps = nothing,
-           make = nothing)
+deploydocs(
+    repo = "github.com/cjprybol/uCSV.jl.git",
+    julia = "0.6")
